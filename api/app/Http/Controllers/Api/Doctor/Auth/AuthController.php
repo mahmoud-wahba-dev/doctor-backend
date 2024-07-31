7ba029083b4a13100b8b147272a40f721ed6fde7
Osama Ibrahim <smsmmarzok30@gmail.com>
2024-08-01 00:52:41 +0300

    login

--- api/app/Http/Controllers/Api/Doctor/Auth/AuthController.php
+++ api/app/Http/Controllers/Api/Doctor/Auth/AuthController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api\Doctor\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Doctor\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -12,6 +15,19 @@ class AuthController extends Controller
         LoginRequest $request,
     )
     {
+        $user = User::where('email',$request->email)->first();
 
+        if(Hash::check($request->password , $user->password)){
+            $token = $user->createToken($user->id)->plainTextToken;
+            return response()->json([
+                'token'=>$token,
+                'message' => __("Loged in Successfully"),
+                'status' => Response::HTTP_OK
+            ], Response::HTTP_OK);
+        }
+        return response()->json([
+            'message' => __("wrong password Or username"),
+            'status' => Response::HTTP_BAD_REQUEST
+        ], Response::HTTP_BAD_REQUEST);
     }
 }
