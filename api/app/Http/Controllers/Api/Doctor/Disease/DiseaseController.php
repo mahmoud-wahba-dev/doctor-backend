8b778b0342889e81052dd48bff8295afe5e21581
Osama Ibrahim <smsmmarzok30@gmail.com>
2024-08-02 15:48:02 +0300

    init controllers

---
+++ api/app/Http/Controllers/Api/Doctor/Disease/DiseaseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Doctor\Disease;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DiseaseController extends Controller
+{
+    //
+}
